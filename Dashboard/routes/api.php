2df5362c35cde41f0808d39d6d4b62b8d3e1afa9
galal husseny <galal.husseny@gmail.com>
2021-12-29 15:07:59 +0200

    day 4 laravel part 2

--- Dashboard/routes/api.php
+++ Dashboard/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\apis\ProductController;
+use App\Http\Controllers\apis\auth\LoginController;
+use App\Http\Controllers\apis\Auth\ProfileController;
+use App\Http\Controllers\apis\Auth\PasswordController;
 use App\Http\Controllers\apis\Auth\RegisterController;
 use App\Http\Controllers\apis\Auth\EmailVerificationController;
 
@@ -22,7 +25,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // requests
-Route::group(['prefix'=>'products'],function(){
+Route::group(['prefix'=>'products','middleware'=>'VerifiedApi'],function(){
     Route::get('/',[ProductController::class,'index']); // method => get , url = 127.0.0.1:8000/api/v1/products , headers => Accept:Application/json
     Route::get('/create',[ProductController::class,'create']);
     Route::get('edit/{id}',[ProductController::class,'edit']);
@@ -33,11 +36,27 @@ Route::group(['prefix'=>'products'],function(){
 
 // authentication token
 Route::group(['prefix'=>'users'],function(){
+
+    // guest
     Route::post('register',RegisterController::class); // invokable controller
-    Route::post('send-code',[EmailVerificationController::class,'sendCode']);
-    Route::post('check-code',[EmailVerificationController::class,'checkCode']);
-    Route::post('email-verification',[EmailVerificationController::class,'emailVerification']);
-    // profile
-    // logout
-    // forget password
+    Route::post('login',[LoginController::class,'login']);
+    Route::post('check-email',[PasswordController::class,'checkEmail']);
+    // auth
+    Route::group(['middleware'=>'auth:sanctum'],function(){
+        Route::post('send-code',[EmailVerificationController::class,'sendCode']);
+        Route::post('check-code',[EmailVerificationController::class,'checkCode']);
+        Route::post('email-verification',[EmailVerificationController::class,'emailVerification']);
+        Route::post('logout',[LoginController::class,'logout']);
+        Route::post('logout-all',[LoginController::class,'logoutAll']);
+    });
+
+    // verified
+    Route::group(['middleware'=>'VerifiedApi'],function(){
+        Route::post('profile',ProfileController::class);
+        Route::post('set-new-password',[PasswordController::class,'setNewPassword']);
+    
+    });
+   
+    
+   
 });
